d46faf478a8b92fca4e043164cbe75a61ea8587a
ashishpagare22 <ashishpagare2210@gmail.com>
2026-08-02 22:15:35 +0530

    Allow Vercel domains in CORS policy

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ app = FastAPI(title="Task Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
